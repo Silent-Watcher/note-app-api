c9e78479862afdf48d1f6c97038fee885ca47d66
Silent-Watcher <alitabatabaee20@gmail.com>
2025-05-25 16:15:57 +0330

    feat(tags): add tags mongo schema model
    define Tag and TagDocument types

---
+++ src/modules/tags/tags.model.ts
@@ -0,0 +1,17 @@
+import { Schema, Types, model } from 'mongoose';
+import type { HydratedDocument, InferSchemaType } from 'mongoose';
+
+const tagSchema = new Schema(
+	{
+		name: { type: String, required: true, trim: true },
+		color: { type: String, required: true, trim: true },
+		parent: { type: Types.ObjectId, ref: 'tags', default: undefined },
+		user: { type: Types.ObjectId, ref: 'users', default: undefined },
+		pinned: { type: Boolean, required: true, default: false },
+	},
+	{ timestamps: false, versionKey: false },
+);
+
+export type Tag = InferSchemaType<typeof tagSchema>;
+export type TagDocument = HydratedDocument<Tag>;
+export const tagModel = model('tag', tagSchema);
