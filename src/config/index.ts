55df53964161bb1a04f7c2279b9457bf900e2e7b
Silent-Watcher <alitabatabaee20@gmail.com>
2025-06-22 17:20:13 +0330

    refactor: add const assertion for app config

--- src/config/index.ts
+++ src/config/index.ts
@@ -11,7 +11,7 @@ export const CONFIG = {
 		VERSION: version,
 		HOST: _env.APP_HOST,
 		URL: `http://${_env.APP_HOST}:${_env.APP_PORT}`,
-	},
+	} as const,
 
 	DB: {
 		HOST: _env.MONGO_HOST,
@@ -20,20 +20,20 @@ export const CONFIG = {
 		PASSWORD: _env.MONGO_PASSWORD,
 		REPLICASET: _env.MONGO_REPLICASET,
 		DB_NAME: _env.MONGO_DATABASE,
-	},
+	} as const,
 
 	REDIS: {
 		HOST: _env.REDIS_HOST,
 		PORT: _env.REDIS_PORT,
 		USERNAME: _env.REDIS_USERNAME,
 		PASSWORD: _env.REDIS_PASSWORD,
-	},
+	} as const,
 
 	SECRET: {
 		REFRESH_TOKEN: _env.REFRESH_TOKEN_SECRET,
 		ACCESS_TOKEN: _env.ACCESS_TOKEN_SECRET,
 		COOKIE: _env.COOKIE_SECRET,
-	},
+	} as const,
 
 	MAX_SESSION_DAYS: 7,
 
@@ -44,20 +44,20 @@ export const CONFIG = {
 		PASS: _env.EMAIL_PASS,
 		FROM_NAME: _env.EMAIL_FROM_NAME,
 		FROM_ADDRESS: _env.EMAIL_FROM_ADDRESS,
-	},
+	} as const,
 
 	CLIENT_BASE_URL: _env.CLIENT_BASE_URL,
 
 	ROUTE: {
 		RESET_PASSWORD: _env.RESET_PASSWORD_ROUTE,
 		LOGIN_PAGE_ROUTE: _env.LOGIN_PAGE_ROUTE,
-	},
+	} as const,
 
 	RECAPTCHA: {
 		SITE_KEY: _env.RECAPTCHA_SITE_KEY,
 		SECRET_KEY: _env.RECAPTCHA_SECRET_KEY,
 		DEV_AUTH: _env.DEV_RECAPTCHA_AUTH,
-	},
+	} as const,
 
 	MINIO: {
 		ACCESS_KEY: _env.MINIO_ACCESS_KEY,
@@ -65,19 +65,19 @@ export const CONFIG = {
 		ENDPOINT: _env.MINIO_ENDPOINT,
 		PORT: _env.MINIO_PORT,
 		USE_SSL: _env.MINIO_USE_SSL,
-	},
+	} as const,
 
 	GITHUB: {
 		CLIENT_ID: _env.GITHUB_CLIENT_ID,
 		CLIENT_SECRET: _env.GITHUB_CLIENT_SECRET,
 		CALLBACK_URL: _env.GITHUB_CALLBACK_URL,
 		STATE_SECRET: _env.GITHUB_STATE_SECRET,
-	},
+	} as const,
 
 	JWT_ACCESS_SECRET: _env.JWT_ACCESS_SECRET,
 
 	SIGHTENGINE: {
 		API_SECRET: _env.SIGHTENGINE_API_SECRET,
 		API_USER: _env.SIGHTENGINE_API_USER,
-	},
+	} as const,
 } as const;
